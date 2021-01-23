3cfa105998e44eeb878a6789da551cc14e8eec29
Christopher Kumm <ChrisLKumm@protonmail.com>
2021-01-23 13:46:01 +0300

    removed semicolons
    Auto-formatted all component .js files, which removed semicolons and replaced double-quotes with single-quotes.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import React, {useEffect, useState} from "react";
-import './global.css';
-import Container from 'react-bootstrap/Container';
-import Piece from "./components/Piece";
-import {useLocation, Link} from 'react-router-dom';
+import React, {useEffect, useState} from 'react'
+import './global.css'
+import Container from 'react-bootstrap/Container'
+import Piece from './components/Piece'
+import {useLocation, Link} from 'react-router-dom'
 
 function App() {
 
-  const [pieces, setPieces] = useState(null);
+  const [pieces, setPieces] = useState(null)
 
   // Get the address in the browser
-  const currentUrl = useLocation();
+  const currentUrl = useLocation()
 
   // Get the API URL
-  let piecesUrl = `${process.env.REACT_APP_DOMAIN}/api${currentUrl.pathname}`;
+  let piecesUrl = `${process.env.REACT_APP_DOMAIN}/api${currentUrl.pathname}`
 
   const loadPieces = () => {
     console.log(`fetching pieces from ${piecesUrl}`)
     fetch(piecesUrl)
       .then(response => response.json())
-      .then(data => setPieces(data));
-  };
+      .then(data => setPieces(data))
+  }
 
-  useEffect(loadPieces, [piecesUrl]);
+  useEffect(loadPieces, [piecesUrl])
 
-  let background;
+  let background
 
   if (pieces) {
     background = `radial-gradient(at 50% 131px, #FFFFFFDD, #FFFFFF00),linear-gradient(#FFFFFF00, white), url(${process.env.REACT_APP_DOMAIN}${pieces[0].image.url})`
   }
 
   return (
-    <div className="App">
+    <div className='App'>
       <header style={{
         backgroundImage: background,
       }}>
@@ -51,7 +51,7 @@ function App() {
         </Container>
       </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css';
-import App from './App';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'normalize.css'
+import App from './App'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -23,5 +23,5 @@ ReactDOM.render(
       </Switch>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
